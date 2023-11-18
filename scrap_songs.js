1cd95052d481c8e90107d794fc1f58f4577506dc
Luzivog <thpfai@gmail.com>
2023-11-18 14:29:43 -0500

    switched local storage to json

--- scrap_songs.js
+++ scrap_songs.js
@@ -1,9 +1,7 @@
 const fs = require('fs');
 const csv = require('csv-parser');
-const { Parser } = require('json2csv');
 const getEmotionsFromQuery = require('./utils/getEmotionsFromQuery');
 const searchSong = require('./utils/searchSong');
-
 
 const readCSV = async (path) => {
     return await new Promise((resolve, reject) => {
@@ -17,17 +15,16 @@ const readCSV = async (path) => {
     });
 };
 
-(async () => {
+import('load-json-file').then(async ({ loadJsonFile }) => {
     file = await readCSV('./MultiLabel.csv');
-    save_path = './data.csv';
-    saved_data = await readCSV(save_path);
+    save_path = './data.json';
+    saved_data = await loadJsonFile(save_path);
     saved_data_set = new Set(saved_data.map(row => row.title));
-    for (const row of saved_data) row.emotions = JSON.parse(row.emotions);
 
     for (const row of file) {
         const infos = await searchSong(row.title, process.env.API_KEY);
         if (saved_data_set.has(infos.title)) {
-            console.log(infos.title + " already in data.csv")
+            console.log(infos.title + " already in data.json")
             continue
         };
 
@@ -35,17 +32,20 @@ const readCSV = async (path) => {
         for (const emotion in emotions) emotions[emotion] = Math.round(emotions[emotion] * 10000)/100;
         infos.emotions = emotions;
         delete infos.lyrics;
-        const fields = Object.keys(infos);
-        let json2csvParser;
 
-        if (fs.existsSync(save_path)) json2csvParser = new Parser({ fields, header: false });
-        else json2csvParser = new Parser({ fields });
-
-        const csv = json2csvParser.parse(infos) + "\n";
-        fs.appendFile(save_path, csv, function(err) {
+        fs.readFile('data.json', 'utf8', (err, data) => {
             if (err) return console.error(err);
+            let jsonData = JSON.parse(data);
+            jsonData.push(infos);
+            console.log(jsonData)
+            const jsonString = JSON.stringify(jsonData, null, 2);
+            fs.writeFile('data.json', jsonString, 'utf8', (err) => {
+                if (err) return console.error(err);
+                console.log('File successfully updated');
+            });
         });
 
         saved_data_set.add(infos.title);
     };
-})();
+}).catch(err => console.error(err));
+
