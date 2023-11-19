933b9f40f6b7e09c8a2a5f8f68d2534b452b00d0
Luzivog <thpfai@gmail.com>
2023-11-19 02:21:08 -0500

    carousel component & start script update

--- src/components/carousel.jsx
+++ src/components/carousel.jsx
@@ -1,10 +1,70 @@
 import React from 'react';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css'; 
+import 'slick-carousel/slick/slick-theme.css';
 
-const Carousel = () => {
+const cardStyles = {
+    container: {
+        width: "50%",
+        paddingBottom: "40px",
+        marginTop: "2%",
+    },
+    card: {
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#fff',
+        borderRadius: '8px',
+        boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2)',
+        display: 'flex',
+        padding: '20px'
+    },
+    image: {
+        width: '80%',
+        height: 'auto',
+        alignSelf: 'center',
+        margin: '0 auto',
+        borderRadius: '8px',
+    },
+    title: {
+        margin: '10px 0',
+        fontSize: '18px',
+        fontWeight: 'bold',
+    },
+    artist: {
+        fontSize: '14px',
+        color: '#555',
+    }
+};
+
+const Carousel = ({songs}) => {
+
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 1500,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        autoplay: true,
+        autoplaySpeed: 5000,
+    };
+
     return (
-        <div className="carousel">
-            Test
-        </div>
+        <div style={cardStyles.container}>
+                <Slider {...settings}>
+                        {songs.slice(0,15).map((song, index) => (
+                            <div key={index} style={cardStyles.card}>
+                                <img 
+                                    src={song.albumArt} 
+                                    alt={`${song.artist_name} album art`} 
+                                    style={cardStyles.image}
+                                />
+                                <h3 style={cardStyles.title}>{song.title}</h3>
+                                <p style={cardStyles.artist}>{song.artist_name}</p>
+                            </div>
+                        ))}
+                </Slider>
+            </div>
     );
 };
 
--- src/pages/SearchPage.jsx
+++ src/pages/SearchPage.jsx
@@ -1,8 +1,7 @@
 import SearchBar from '../components/searchbar';
-import Slider from 'react-slick'; // Import the carousel component
-import 'slick-carousel/slick/slick.css'; 
-import 'slick-carousel/slick/slick-theme.css';
 import songs from '../data.json';
+import Carousel from '../components/carousel';
+
 
 const styles = {
     mainContainer: {
@@ -10,20 +9,15 @@ const styles = {
         flexDirection: 'column',
         alignItems: 'center',
         justifyContent: 'center',
-        height: '100vh', // Full view height
-        overflow: 'hidden', // Prevent scrolling
+        height: '100vh',
+        overflow: 'hidden',
     },
     searchContainer: {
         width: "50%",
         backgroundColor: 'rgba(255, 255, 255, 0.4)',
         backdropFilter: 'blur(10px)',
         borderRadius: '8px',
-        padding: "40px 0",
-    },
-    carouselContainer: {
-        width: "50%", // Set the width to 50% to match the searchContainer
         paddingBottom: "40px",
-        marginTop: "2%",
     },
     title: {
         fontSize: "75px",
@@ -32,48 +26,7 @@ const styles = {
     },
 };
 
-
-const cardStyles = {
-    card: {
-        flexDirection: 'column',
-        alignItems: 'center',
-        justifyContent: 'center',
-        backgroundColor: '#fff',
-        borderRadius: '8px',
-        boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2)',
-        display: 'flex',
-        padding: '20px'
-    },
-    image: {
-        width: '80%',
-        height: 'auto',
-        alignSelf: 'center',
-        margin: '0 auto',
-        borderRadius: '8px',
-    },
-    title: {
-        margin: '10px 0',
-        fontSize: '18px',
-        fontWeight: 'bold',
-    },
-    artist: {
-        fontSize: '14px',
-        color: '#555',
-    }
-};
-
-
 const Home = () => {
-
-    const settings = {
-        dots: true,
-        infinite: true,
-        speed: 1000,
-        slidesToShow: 3,
-        slidesToScroll: 3,
-        autoplay: true,
-        autoplaySpeed: 3000,
-    };
 
     return (
         <div style={styles.mainContainer}>
@@ -81,21 +34,7 @@ const Home = () => {
                 <h1 style={styles.title}>Search Jam</h1>
                 <SearchBar />
             </div>
-            <div style={styles.carouselContainer}>
-                <Slider {...settings}>
-                        {songs.slice(0,15).map((song, index) => (
-                            <div key={index} style={cardStyles.card}>
-                                <img 
-                                    src={song.albumArt} 
-                                    alt={`${song.artist_name} album art`} 
-                                    style={cardStyles.image}
-                                />
-                                <h3 style={cardStyles.title}>{song.title}</h3>
-                                <p style={cardStyles.artist}>{song.artist_name}</p>
-                            </div>
-                        ))}
-                </Slider>
-            </div>
+            <Carousel songs={songs} />
         </div>
     );
 }
